bceb47b3f8b037cc2f40d1b1e00e3efde8d65241
microspaze <microspaze@qq.com>
2024-06-19 11:13:31 +0800

    Include WebSocketSharp codes.

---
+++ FlutterBridge.Maui/Attributes/BridgeActionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlutterBridge.Maui.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class BridgeActionAttribute : Attribute
+    {
+    }
+}
---
+++ FlutterBridge.Maui/Attributes/BridgeEventAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlutterBridge.Maui.Attributes
+{
+    [AttributeUsage(AttributeTargets.Event)]
+    public sealed class BridgeEventAttribute : Attribute
+    {
+    }
+}
